d92b841d184eaf24717abd29f36dec2408df6a9a
Luke Ding <lding@pdx.edu>
2019-11-13 13:04:19 -0800

    Assignment 6
    Assignment 6

--- Assignment5/app.js
+++ Assignment5/app.js
@@ -1,39 +1,19 @@
-const express = require ("express");
-const app = express();
-app.use(express.json());
+var express = require('express');
+var app = express();
 
-let feedback = [
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-];
-
-app.get('/', (req, res) => {
-    res.send(feedback);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/api/courses/:id', (req, res) => {
-   const course = courses.find(c => c.id === parseInt(req.params.id));
-   if (!course) res.status(404).send('Request was not found');
-   res.send(course);
-
+app.post('/submit-student-data', function (req, res) {
+    var name = 'Name: ' + req.body.NameInput + ' Email: ' + req.body.EmailInput + ' Comment: ' + req.body.Comments;
+    
+    res.send(name);
 });
 
-app.post('/submit', (req, res) => {
-   const inputs = {
-        id : feedback.length + 1,
-        name: req.body.name,
-        email: req.body.email,
-        comment: req.body.comment
-   };
-   feedback.push(inputs);
-   res.send(inputs);
- });
-
-
-//PORT
-
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-
-const port = 3000;
-app.listen(3000, () => console.log('Listening on port ...' + port));
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+}); 
